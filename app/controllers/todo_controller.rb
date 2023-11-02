a0a71e54275646507c8ae53e1dad248681a9c1e9
Enmanuel_Q <qenmanuel2@gmail.com>
2023-11-02 14:57:55 -0600

    002 - back description movie avanced

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,7 +7,7 @@ class TodoController < ApplicationController
     #movie_id = params[:id]  # El ID de la película lo obtienes desde la URL
     api_key = '7752de1a342e0930da1c72487148b06b'  # Reemplaza con tu clave de API de TMDb
 
-    url = URI.parse("https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}")
+    url = URI.parse("https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=es-ES")
     @link_img='https://image.tmdb.org/t/p/w500'
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true if url.scheme == 'https'
@@ -22,4 +22,37 @@ class TodoController < ApplicationController
       @error_message = "Error: #{response.code}"
     end
   end
+
+  def descripcion
+    api_key = '7752de1a342e0930da1c72487148b06b'
+    movie_id = params[:parametro]
+    @link_img = 'https://image.tmdb.org/t/p/w500'
+    
+    # Obtener información de la película
+    url = URI.parse("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{api_key}&language=es-ES")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true if url.scheme == 'https'
+    request = Net::HTTP::Get.new(url.request_uri)
+    response = http.request(request)
+    
+    if response.code == '200'
+      @info = JSON.parse(response.body)
+    else
+      @error_message = "Error: #{response.code}"
+    end
+    
+    # Obtener información de los videos
+    url_img = URI.parse("https://api.themoviedb.org/3/movie/#{movie_id}/videos?api_key=#{api_key}")
+    http = Net::HTTP.new(url_img.host, url_img.port)
+    http.use_ssl = true if url_img.scheme == 'https'
+    request = Net::HTTP::Get.new(url_img.request_uri)
+    response = http.request(request)
+    
+    if response.code == '200'
+      @info_videos = response.body  # No se necesita parsear, ya es un string JSON
+    else
+      @error_message = "Error: #{response.code}"
+    end
+  end
+  
 end
