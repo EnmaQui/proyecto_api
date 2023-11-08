a359c017223c987e0e44936ab41535d3cb9a126a
qenmanuel2@gmail.com <nicoleto567@gmail.com>
2023-11-07 20:31:58 -0600

    107 - aplication of search complete  c and v

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -80,7 +80,7 @@ class TodoController < ApplicationController
 
   def index
     api_key = '7752de1a342e0930da1c72487148b06b'
-    url = URI.parse("https://api.themoviedb.org/3/movie/top_rated?api_key=#{api_key}")
+    url = URI.parse("https://api.themoviedb.org/3/movie/top_rated?api_key=#{api_key}&language=es-ES")
     @link_img='https://image.tmdb.org/t/p/w500'
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true if url.scheme == 'https'
@@ -95,47 +95,28 @@ class TodoController < ApplicationController
     end
   end
   
-
-  def prueba
-    api_key = '7752de1a342e0930da1c72487148b06b'
-    url = URI.parse("https://api.themoviedb.org/3/movie/top_rated?api_key=#{api_key}")
-    @link_img='https://image.tmdb.org/t/p/w500'
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true if url.scheme == 'https'
-    request = Net::HTTP::Get.new(url.request_uri)
-    response = http.request(request)
-    
-    if response.code == '200'
-      movies = JSON.parse(response.body)['results']
-      page = params[:page] || 1
-      per_page = 10
-      @nuevas = Kaminari.paginate_array(movies).page(page).per(per_page) # Pagina los resultados, 10 por página
-    else
-      @error_message = "Error: #{response.code}"
-    end
-  end
-
   
 
   def resultado
     #'https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&include_adult=true&language=es-ES&page=1' \
     api_key = '7752de1a342e0930da1c72487148b06b'
     @link_img = 'https://image.tmdb.org/t/p/w500'
-    #query= params[:nombre]
-    query= "saw"
+    query_i = params[:query]
+    peli=query_i.to_s.gsub(/\s/, '%20')
+  
     # Obtener información de la película
-    url = URI.parse("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{query}&include_adult=true&language=es-ES&page=1")
+    url = URI.parse("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{peli}&include_adult=true&language=es-ES&page=1")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true if url.scheme == 'https'
     request = Net::HTTP::Get.new(url.request_uri)
     response = http.request(request)
     
     if response.code == '200'
-      @info = JSON.parse(response.body)
+      @info = JSON.parse(response.body)['results']
     else
       @error_message = "Error: #{response.code}"
     end
-    
+
   end
 
 
