7933beccf2f81ff562dbec51691ba50839f9949e
keny <kennylopez24012003@gmail.com>
2023-11-08 15:12:14 -0600

    test terminados

--- spec/prueba_spec.rb
+++ spec/prueba_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller do
-  describe "GET #homepage" do
-    it "renders the 'homepage' template" do
-      get :homepage
-      expect(response).to render_template("homepage")
+RSpec.describe TodoController, type: :controller do
+    describe "GET #homepage" do
+      it "renders the 'homepage' template" do
+        get :homepage
+        expect(response).to render_template("homepage")
+      end
     end
   end
-end
